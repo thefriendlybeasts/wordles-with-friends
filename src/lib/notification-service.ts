65c4a77323aefe543443f45da8a8eddda0d15228
Curtis Blackwell <blackwell.curtis@gmail.com>
2022-04-25 18:40:06 -0500

    Clearer alert messages
    Co-authored-by: Trevor Stinson <trevor@trevorstinson.com>

--- src/lib/notification-service.ts
+++ src/lib/notification-service.ts
@@ -1,10 +1,10 @@
 export default class NotificationService {
   alertInvalidWordError() {
-		alert(`There's a problem with your word. Ensure it's 5 letters long and is a word.`);
+		alert(`There's a problem with your word. Ensure it's 5 letters long and is a word according to our dictionary.`);
 	}
 
 	alertUnknownError(response) {
-		alert('There was an unknown error. If you know what it means, check the console.');
+		alert('There was an unknown error. If you know what "check the console" means, do it.');
 		console.dir(response);
 	}
 }
